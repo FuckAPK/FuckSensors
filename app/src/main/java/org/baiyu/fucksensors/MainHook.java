e7a413b78f877d98f2b179ac9aea0b9c98477c13
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-09-23 20:42:29 +0800

    update

--- app/src/main/java/org/baiyu/fucksensors/MainHook.java
+++ app/src/main/java/org/baiyu/fucksensors/MainHook.java
@@ -2,8 +2,6 @@ package org.baiyu.fucksensors;
 
 import android.hardware.Sensor;
 
-import java.util.Set;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
@@ -11,26 +9,22 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class MainHook implements IXposedHookLoadPackage{
 
-    private static final Set<Integer> sensorsList = Set.of(
-            Sensor.TYPE_ACCELEROMETER,
-            Sensor.TYPE_GRAVITY,
-            Sensor.TYPE_GYROSCOPE,
-            Sensor.TYPE_LINEAR_ACCELERATION
-            );
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        Class<?> sensorManager = XposedHelpers.findClassIfExists("android.hardware.SensorManager ", lpparam.classLoader);
+        if (sensorManager == null) {
+            return;
+        }
         XposedBridge.hookAllMethods(
-                XposedHelpers.findClass("android.hardware.SensorManager ", lpparam.classLoader),
+                sensorManager,
                 "registerListener",
                 new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                         super.beforeHookedMethod(param);
                         if (param.args[1] instanceof Sensor s) {
-                            if (sensorsList.contains(s.getType())) {
-                                param.setResult(true);
-                                XposedBridge.log("Sensor Fucked: " + lpparam.packageName + ": " + s.getName() + ", " + s.getStringType());
-                            }
+                            param.setResult(true);
+                            XposedBridge.log("Sensor Fucked: " + lpparam.packageName + ": " + s.getName() + ", " + s.getStringType());
                         }
                     }
                 }
