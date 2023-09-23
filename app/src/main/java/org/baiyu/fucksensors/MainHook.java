8f07afbe21f6757df350223e4000f90c5ca6ceca
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-09-23 20:48:42 +0800

    update

--- app/src/main/java/org/baiyu/fucksensors/MainHook.java
+++ app/src/main/java/org/baiyu/fucksensors/MainHook.java
@@ -11,23 +11,23 @@ public class MainHook implements IXposedHookLoadPackage{
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        Class<?> sensorManager = XposedHelpers.findClassIfExists("android.hardware.SensorManager ", lpparam.classLoader);
-        if (sensorManager == null) {
-            return;
-        }
-        XposedBridge.hookAllMethods(
-                sensorManager,
-                "registerListener",
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        super.beforeHookedMethod(param);
-                        if (param.args[1] instanceof Sensor s) {
-                            param.setResult(true);
-                            XposedBridge.log("Sensor Fucked: " + lpparam.packageName + ": " + s.getName() + ", " + s.getStringType());
+        try {
+            XposedBridge.hookAllMethods(
+                    XposedHelpers.findClass("android.hardware.SensorManager", lpparam.classLoader),
+                    "registerListener",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            super.beforeHookedMethod(param);
+                            if (param.args[1] instanceof Sensor s) {
+                                param.setResult(true);
+                                XposedBridge.log("Sensor Fucked: " + lpparam.packageName + ": " + s.getName() + ", " + s.getStringType());
+                            }
                         }
                     }
-                }
-        );
+            );
+        } catch (XposedHelpers.ClassNotFoundError e) {
+            XposedBridge.log("Fuck Sensors: " + lpparam.packageName + ": android.hardware.SensorManager class not found");
+        }
     }
 }
